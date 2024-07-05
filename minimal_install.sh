6c5021415d1d09747419c8aca35c8f408bd3c79a
Kvn11 <45984541+Kvn11@users.noreply.github.com>
2024-07-04 20:39:54 -0400

    Update minimal_install.sh
    updates

--- minimal_install.sh
+++ minimal_install.sh
@@ -2,6 +2,7 @@
 
 current_user="$USER"
 mkdir -p ~/.config
+mkdir -p ~/.config/alacritty
 
 sudo apt update
 sudo apt install -y \
@@ -38,6 +39,8 @@ eval "$(ssh-agent -s)"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 # Install golang
+cd /home/$current_user/Downloads
+wget https://go.dev/dl/go1.22.5.linux-amd64.tar.gz
 rm -rf /usr/local/go && tar -C /usr/local -xzf go1.22.5.linux-amd64.tar.gz
 echo 'PATH=$PATH:/usr/local/go/bin' >> ~/.zshrc
 export PATH=$PATH:/usr/local/go/bin
@@ -51,4 +54,4 @@ fc-cache -f -v
 curl -sS https://starship.rs/install.sh | sh
 
 # Set theme to custom dracula:
-wget https://raw.githubusercontent.com/Kvn11/dotfiles/master/alacritty/alacritty.toml -O ~/.config/.alacritty.toml
+wget https://raw.githubusercontent.com/Kvn11/dotfiles/master/alacritty/alacritty.toml -O ~/.config/alacritty/alacritty.toml
